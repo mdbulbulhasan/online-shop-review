ad767fcbb69aaa8332fe6146da57aafdb32cd470
mdbulbulhasan <bulbulhasan095@gmail.com>
2025-08-09 01:53:39 +0600

    add utils for handle data fetch from localstorage

---
+++ src/utils/reviewstored.js
@@ -0,0 +1,24 @@
+const STORAGE_KEY = "reviews";
+
+export function getReviews() {
+  const storedReview = localStorage.getItem(STORAGE_KEY);
+  return storedReview ? JSON.parse(storedReview) : [];
+}
+
+export function saveReviews(reviews) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(reviews));
+}
+
+export function addReview(newReview) {
+  const currentReviews = getReviews();
+  const updated = [newReview, ...currentReviews];
+  saveReviews(updated);
+  return updated;
+}
+
+export function deleteReviewById(id) {
+    const currentReviews = getReviews();
+    const updated = currentReviews.filter(review => review.id !== id);
+    saveReviews(updated);
+    return updated;
+}
